f476d80e84d354058504996d0eeb6ef88427ad7d
Yanina Hernandez <yanina.hernandez@deepsource.cl>
2023-06-07 00:12:23 -0400

    cors

--- src/main/java/com/shop/geekbox/controller/ProductoOfertaController.java
+++ src/main/java/com/shop/geekbox/controller/ProductoOfertaController.java
@@ -1,6 +1,10 @@
 package com.shop.geekbox.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,19 @@ public class ProductoOfertaController {
 
     @Autowired
     ProductoOfertaRepository productoOfertaRepository;
-    
+
     @PostMapping(value = "")
     public ProductoOferta save(@RequestBody ProductoOferta productoOferta) {
         return productoOfertaRepository.save(productoOferta);
+    }
+
+    @GetMapping(value = "")
+    public Iterable<ProductoOferta> get() {
+        return productoOfertaRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ProductoOferta getById(@PathVariable("id") Integer id) {
+        return productoOfertaRepository.findById(id).get();
     }
 }
--- src/main/java/com/shop/geekbox/model/ProductoOferta.java
+++ src/main/java/com/shop/geekbox/model/ProductoOferta.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,6 +33,7 @@ public class ProductoOferta {
     @Column(name="CDescProdOfer")
     private String cdescProdOfer;
 
-    @Column(name="Persona_IIdPer")
-    private Integer persona_IIdPer;
+    @ManyToOne
+    @JoinColumn(name = "Persona_IIdPer")
+    private Persona persona;
 }
