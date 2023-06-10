11d6fbc2477a93051bf843a4acdefbf62bb69a61
Yanina Hernandez <yanina.hernandez@deepsource.cl>
2023-06-10 01:07:26 -0400

    add cross origin

--- src/main/java/com/shop/geekbox/controller/LoginController.java
+++ src/main/java/com/shop/geekbox/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.shop.geekbox.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shop.geekbox.dto.LoginDTO;
 import com.shop.geekbox.repository.PersonaRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/login")
 public class LoginController {
@@ -16,8 +19,10 @@ public class LoginController {
     @Autowired
     PersonaRepository personaRepository;
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "")
-    public boolean login(@RequestBody LoginDTO login) {
-        return personaRepository.findByCemailAndCpass(login.getMail(), login.getPassword()) != null? true: false;
+    public ResponseEntity<Boolean> login(@RequestBody LoginDTO login) {
+        Boolean response = personaRepository.findByCemailAndCpass(login.getMail(), login.getPassword()) != null? true: false;
+        return ResponseEntity.ok().body(response);
     }
 }
